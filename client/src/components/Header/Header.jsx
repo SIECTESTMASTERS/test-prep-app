24ced5f2fe085fcaa322e8447f1e326cbf6dd6df
Sharon Mwenda <smkathambi@gmail.com>
2023-03-14 16:20:34 +0300

    feat:built navbar component with logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Button, Container } from 'reactstrap';
+import logo from '../../images/sieclogo.png'
+import './header.css'
+
+function createData(display, url){
+  return { display, url}
+}
+
+const navLinks = [
+  createData('Home', '#'),
+  createData('About', '#'),
+  createData('Courses', '#')
+]
 
 function Header() {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu")
+
   return (
-    <div>
-      
-    </div>
+   <header className='header'>
+    <Container>
+      <div className="navigation d-flex align-items-center justify-content-between">
+        <div className="logo">
+          <img src={logo} height='35' width='35'/>
+          <h2 d-flex align-items-center gap-1>Test Masters</h2>
+        </div>
+
+        <div className="nav d-flex align-items-center gap-5">
+          <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+            <ul className="nav__list">
+              {navLinks.map((item)=> (
+              <li key={item.display} className="nav__item">
+                <a href={item.url}>{item.display}</a>
+              </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="nav__right">
+            <Button>
+              Register
+            </Button>
+            <Button>
+              Login
+            </Button>
+          </div>
+        </div>
+
+        <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div> 
+      </div>
+    </Container>
+   </header>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 
 function Home() {
   return (
     <div>
-      Hello world
+      <Header />
     </div>
   )
 }
